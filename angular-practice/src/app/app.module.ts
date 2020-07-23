e0908bc176b0c7f6e528e7b7d9dedd7d167f4fde
Jordan Gamble <jgamble482@gmail.com>
2020-07-23 12:37:58 -0400

    Highlight Component Complete

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightComponent } from './highlight/highlight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightComponent
   ],
   imports: [
     BrowserModule,
